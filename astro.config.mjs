4cd1399709e5cdf1b1770f20947d5b8cb1ebb3db
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2023-03-22 19:33:32 +0100

    create base layout with central config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
 
+import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'http://gohector.com',
   integrations: [sitemap(), robotsTxt()]
 });
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+
+export default {
+    author: "Hector Castelli Zacharias",
+    description: "My personal website",
+    keywords: ["blog", "portfolio", "personal"]
+
+}
