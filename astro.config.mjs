1d8e6aebcfdf4776909be8f450c29d9ee6fa8ec3
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2023-03-22 20:09:39 +0100

    start defining landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'http://gohector.com',
-  integrations: [sitemap(), robotsTxt()]
+  integrations: [sitemap(), robotsTxt(), tailwind()]
 });
