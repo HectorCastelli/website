9f439a808c9555a110f0762ea9e493f0aa8cb13d
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2023-03-23 20:52:36 +0100

    implement dark mode add global announcement banner

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@
 export default {
     author: "Hector Castelli Zacharias",
     description: "My personal website",
-    keywords: ["blog", "portfolio", "personal"]
-
+    keywords: ["blog", "portfolio", "personal"],
+    announcement: undefined,
+    // announcement: "This is my announcement text that should be in all pages!"
 }
