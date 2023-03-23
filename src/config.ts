947a0425beee19485ccb8ba20f84009c4f0fb931
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2023-03-23 21:02:06 +0100

    add in-progress banner

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ export default {
     author: "Hector Castelli Zacharias",
     description: "My personal website",
     keywords: ["blog", "portfolio", "personal"],
-    announcement: undefined,
-    // announcement: "This is my announcement text that should be in all pages!"
+    // announcement: undefined,
+    announcement: "This website is under construction. There is a high change some links will not work."
 }
