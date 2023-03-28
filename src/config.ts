d52da43bec3b1fd9d6ce24c08bd92365bc5f7d3b
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2023-03-28 21:08:18 +0200

    add header and footer to generic pages

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,42 @@
+const CNAME = "https://gohector.com"
 
 export default {
+    cname: CNAME,
     author: "Hector Castelli Zacharias",
     description: "My personal website",
     keywords: ["blog", "portfolio", "personal"],
     // announcement: undefined,
-    announcement: "This website is under construction. There is a high change some links will not work."
+    announcement: "This website is under construction. There is a high change some links will not work.",
+    header: {
+        links: [
+            {
+                text: "About me",
+                href: new URL(`${CNAME}/about`)
+            },
+            {
+                text: "Projects",
+                href: new URL(`${CNAME}/projects`)
+            },
+            {
+                text: "Blog",
+                href: new URL(`${CNAME}/blog`)
+            },
+            {
+                text: "Contact",
+                href: new URL(`${CNAME}/contact`)
+            },
+        ]
+    },
+    footer: {
+        links: [
+            {
+                text: "Homepage",
+                href: new URL(`${CNAME}/`)
+            },
+            {
+                text: "Feed (RSS)",
+                href: new URL(`${CNAME}/feed.rss`)
+            },
+        ]
+    }
 }
