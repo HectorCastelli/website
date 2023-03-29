da151bc3ee2d74c84b92fd13c48a2dd25f5ffb7f
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2023-03-29 20:41:47 +0200

    add script to create blog entry

--- README.md
+++ README.md
@@ -17,3 +17,5 @@ All commands are run from the root of the project, from a terminal:
 | `npm run preview`      | Preview your build locally, before deploying     |
 | `npm run astro ...`    | Run CLI commands like `astro add`, `astro check` |
 | `npm run astro --help` | Get help using the Astro CLI                     |
+| `npm run blog-create`  | Create a new entry in the blog with today's date |
+
---
+++ scripts/blog/create.js
@@ -0,0 +1,27 @@
+import fs from 'node:fs';
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+console.log("Let's create a new blog entry")
+
+const rl = readline.createInterface({ input, output });
+
+const title = await rl.question('What is the title of the post? ');
+
+const fileContents = `---
+title: "${title}"
+pubDate: "${new Date().toISOString().substring(0,10)}"
+description: ""
+tags: []
+---
+`
+
+try {
+  fs.writeFileSync(`./src/content/blog/${title.toLocaleLowerCase().replaceAll(/[^a-z]/ig,'-')}.md`, fileContents);
+  console.log('Post created with success')
+  // file written successfully
+} catch (err) {
+  console.error(err);
+}
+
+rl.close();
