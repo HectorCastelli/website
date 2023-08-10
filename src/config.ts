084f95d3b5780330dd64947ebdfd827dee78873c
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-08-10 21:32:51 +0200

    remove announcement. website should be functional

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export default {
     author: "Hector Castelli Zacharias",
     description: "My personal website",
     keywords: ["blog", "portfolio", "personal"],
-    // announcement: undefined,
-    announcement: "This website is under construction. There is a high chance some links will not work.",
+    announcement: undefined,
+    // announcement: "",
     header: {
         links: [
             {
