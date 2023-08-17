1330ba72478691c9661c8f6c3da90202e81ba943
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-08-17 23:02:57 +0200

    fix unused code

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,15 +1,13 @@
 import config from "@config";
-import reportAccessibilityViolations from "../support/accessibility";
-
 
 describe("index page", () => {
-  let page;
+  let page
   beforeEach(() => {
-    page = cy.visit('/');
+    page = cy.visit('/')
   })
 
   it('titles are correct', () => {
-    page.get('title').should('have.text', config.title);
+    page.get('title').should('have.text', config.title)
   });
 
   it('Has no detectable a11y violations on load', () => {
