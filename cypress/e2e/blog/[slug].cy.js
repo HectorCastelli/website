7c593cc8e7179529821a380c9c775d6e76747c6a
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-08-30 21:37:45 +0200

    add test for social images

--- cypress/e2e/blog/[slug].cy.js
+++ cypress/e2e/blog/[slug].cy.js
@@ -7,6 +7,20 @@ describe("blog pages", () => {
             beforeEach(() => {
                 page = cy.visit(`/blog/${post}`)
             })
+            it('has social media image', () => {
+                page.get('head meta[property=og:image]')
+                    .should('have.attr', 'content')
+                    .should('include', 'opengraph/')
+                page.get('head meta[name=twitter:card]')
+                    .should('have.attr', 'content')
+                page.get('head meta[name=twitter:image]')
+                    .should('have.attr', 'content')
+                    .should('include', 'opengraph/')
+
+                page.get('head meta[property=og:image]')
+                    .invoke('attr', 'content')
+                    .then((imagePath) => cy.request(imagePath).it('status').should('eq', 200))
+            })
             it('has no detectable a11y violations on load', () => {
                 cy.checkAccessibility()
             })
