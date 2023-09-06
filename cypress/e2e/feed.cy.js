aeb79fb353b97182d1498a0a7c1c255b215368e6
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-09-06 11:41:22 +0200

    fix feed tests

--- cypress/e2e/feed.cy.js
+++ cypress/e2e/feed.cy.js
@@ -3,14 +3,10 @@ import config from "@config";
 describe("RSS feed", () => {
   let page
   beforeEach(() => {
-    page = cy.visit('/rss.xml')
+    page = cy.request('/rss.xml')
   })
 
   it('titles are correct', () => {
     page.get('title').should('have.text', config.title)
   });
-
-  it('has no detectable a11y violations on load', () => {
-    cy.checkAccessibility()
-  })
 })
