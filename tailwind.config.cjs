9b09575736d3b62a08296308c3decf88b7ce90a4
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-10-17 14:49:09 +0200

    use color aliases

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: colors.green,
+				secondary: colors.emerald,
+				neutral: colors.stone,
+			},
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
