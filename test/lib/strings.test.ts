6edad0a69eafb0dfad7fc1eb6e1468549e78a0af
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-10-19 02:06:08 +0200

    add functions to handle dates in resumes

--- test/lib/strings.test.ts
+++ test/lib/strings.test.ts
@@ -1,24 +1,32 @@
-import { expect, test } from 'vitest';
-import { chunkString } from '@lib/strings';
+import { describe, expect, test } from 'vitest';
+import { chunkString, dateToMonthString } from '@lib/strings';
 
-test('Works on empty strings', () => {
-    expect(chunkString("", 1)).toStrictEqual([]);
+describe("String chunking", () => {
+    test('Works on empty strings', () => {
+        expect(chunkString("", 1)).toStrictEqual([]);
+    });
+
+    test('Cannot chunk single words', () => {
+        expect(chunkString("word", 5)).toStrictEqual([
+            "word"
+        ]);
+    });
+
+    test('Chunks on whitespace', () => {
+        expect(chunkString("one two", 3)).toStrictEqual([
+            "one", "two"
+        ]);
+    });
+
+    test('Chunks early if impossible size', () => {
+        expect(chunkString("must chunk", 3)).toStrictEqual([
+            "must", "chunk"
+        ]);
+    });
 });
 
-test('Cannot chunk single words', () => {
-    expect(chunkString("word", 5)).toStrictEqual([
-        "word"
-    ]);
-});
-
-test('Chunks on whitespace', () => {
-    expect(chunkString("one two", 3)).toStrictEqual([
-        "one", "two"
-    ]);
-});
-
-test('Chunks early if impossible size', () => {
-    expect(chunkString("must chunk", 3)).toStrictEqual([
-        "must", "chunk"
-    ]);
+describe("Date to String conversion", () => {
+    test('Uses the correct months', () => {
+        expect(dateToMonthString(new Date('2023-01-01'))).toStrictEqual("January 2023")
+    });
 });
