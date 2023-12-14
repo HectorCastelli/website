c639a0d611abbbcaea89bce7cf2dec9e00486e5e
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2023-12-14 22:39:29 +0000

    Talk about the death of the DevOps philosophy
    Signed-off-by: Héctor Castelli Zacharias <hector.zacharias@gmail.com>

---
+++ src/content/blog/devops-is-dead.m
@@ -0,0 +1,19 @@
+---
+title: "DevOps is dead, long live DevOps!"
+pubDate: 2023-12-14
+description: "What happened to the DevOps philosophy and where is it going in the corporate world?"
+tags: ['software', 'career']
+---
+
+DevOps is now "platform engieneering"
+
+The point where "released" was has moved from "before the ops teams handles it" to "before the platform handles it"
+
+Platforms are now hailed as the one-stop solution for speed problems with software delivery, but the promises are the same as devops.... is it all a lie? Probably.
+
+Concept of cutting jobs by centralizing concerns (devops meant devs would absorb ops; platforms mean a group o devs deduplicates efforts accross other teams) is too tempting for companies. Software development doesnt scale in the same way as other things.
+This is mostly a pipe-dream and companies should realize that software is not an assembly line, the same ideas dont apply.
+
+However, platforms are good. And so is devops. The key is balance between "size of team", "size of problem", and "expertise of team".
+
+TODO: Make a chart and describe operating modes for the extremes of these three axis.
